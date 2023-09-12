7ed214ee0b75c913ee5b4943c3d04a9d28047f11
kervinazo5 <kervinazo5@gmail.com>
2023-09-12 10:47:03 -0400

    update

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -19,34 +19,34 @@ const Contact = () => {
     };
   return (
     <section id="contact">
-        <h5 className='contact__title-primary'>Get In Touch</h5>
-        <h2 className='contact__title'>Contact Me</h2>
+        <h5 className="contact__title-primary">Get In Touch</h5>
+        <h2 className="contact__title">Contact Me</h2>
         <div className="container contact__container">
             <div className="contact__options">
                 <article className="contact__option">
-                    <MdOutlineEmail className='contact__option-icon'/>
+                    <MdOutlineEmail className="contact__option-icon"/>
                     <h4>Email</h4>
                     <h5>Email@gmail.com</h5>
-                    <a href="mailto:email@gmail.com" target='_blank'>Send email</a>
+                    <a href={"mailto:email@gmail.com"} target='_blank' rel="noreferrer">Send email</a>
                 </article>
                 <article className="contact__option">
-                    <RiMessengerLine className='contact__option-icon'/>
+                    <RiMessengerLine className="contact__option-icon"/>
                     <h4>Messager</h4>
                     <h5>Email@gmail.com</h5>
-                    <a href="https://m.me/ernest.achiever" target='_blank'>Send email</a>
+                    <a href={"https://m.me/ernest.achiever"} target='_blank' rel="noreferrer">Send Messager</a>
                 </article>
                 <article className="contact__option">
                     <BsWhatsapp className='contact__option-icon'/>
                     <h4>WhatsApp</h4>
                     <h5>+1 4045948866</h5>
-                    <a href="https://api.whatsapp.com/send?phone+584120882697" target='_blank'>Send Whatsapp</a>
+                    <a href={"https://api.whatsapp.com/send?phone=%2B14045948866"} target='_blank' rel="noreferrer">Send Whatsapp</a>
                 </article>
             </div>
             <form ref={form} onSubmit={sendEmail}>
-                    <input type="text" name='name' placeholder='Your Full Name' required/>
-                    <input type="email" name='email'  placeholder='Your email' required/>
-                    <textarea name="message" rows="7" placeholder='Your message' required></textarea>
-                    <button type='submit' className='btn btn-primary'>Send Message</button>
+                    <input type="text" name="name" placeholder="Your Full Name" required/>
+                    <input type="email" name="email"  placeholder="Your email" required/>
+                    <textarea name="message" rows="7" placeholder="Your message" required></textarea>
+                    <button type="submit" className="btn btn-primary">Send Message</button>
                 </form>
         </div>
     </section>
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -8,7 +8,7 @@ import {SiGooglemaps}  from 'react-icons/si';
 
 const Header = () => {
   return (
-    <section id='#'>
+    <section id="#">
       <header>
       <div className="container header__container">
           <h1 className="header__title">Martin's Fast Automotive <br/> Service</h1>
@@ -16,13 +16,13 @@ const Header = () => {
         
 
           <div className="header__socials">
-              <a href={'https://www.facebook.com/martintprada?mibextid=ZbWKwL'} target="_blank" rel='noreferrer'><BsFacebook/></a>
-              <a href={'https://api.whatsapp.com/send?phone=%2B14045948866'} target="_blank" rel='noreferrer'><BsWhatsapp/></a>
-              <a href={'https://instagram.com/martinsfastauto?utm_source=qr&igshid=NGExMmI2YTkyZg%3D%3D'} target="_blank" rel='noreferrer'><BsInstagram/></a>
-              <a href={'https://maps.google.com/?cid=3376485699300949514&entry=gps'} target="_blank" rel='noreferrer'><SiGooglemaps/></a>
+              <a href={'https://www.facebook.com/martintprada?mibextid=ZbWKwL'} target="_blank" rel="noreferrer"><BsFacebook/></a>
+              <a href={'https://api.whatsapp.com/send?phone=%2B14045948866'} target="_blank" rel="noreferrer"><BsWhatsapp/></a>
+              <a href={'https://instagram.com/martinsfastauto?utm_source=qr&igshid=NGExMmI2YTkyZg%3D%3D'} target="_blank" rel="noreferrer"><BsInstagram/></a>
+              <a href={'https://maps.google.com/?cid=3376485699300949514&entry=gps'} target="_blank" rel="noreferrer"><SiGooglemaps/></a>
           </div>
         <div className="btn__container">
-          <button className='btn btn-primary'>Contact</button>
+          <button className="btn btn-primary">Contact</button>
         </div>
       </div>
       <div className="header__decorator-1">
